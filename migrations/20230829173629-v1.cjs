8a3f76fcee60648210489f6cc07c20d521be5acb
ArnaudCHEVALME <arnaud.chevalme@proton.me>
2023-08-29 20:10:43 +0200

    reset migration

--- migrations/20230829173629-v1.cjs
+++ migrations/20230829173629-v1.cjs
@@ -3,15 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('clients', 'clientUUID', {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      allowNull: false,
-      unique: true,
-    });
+
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('clients', 'clientUUID');
+
   }
 };
