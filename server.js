93be320176ff37565d1d3418f339273e086b63b1
ArnaudCHEVALME <arnaud.chevalme@proton.me>
2023-08-30 20:28:12 +0200

    add cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import yaml from 'yaml';
 const app = express();
 
 const corsOptions = {
-  // origin: "http://localhost:8081",
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
@@ -22,13 +22,13 @@ const HOST = process.env._ABII_API_HOST;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "x-access-token, Origin, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
 
 // Use routes defined in backend/routers
 for (const createRouter of createRouterFunctions) {
