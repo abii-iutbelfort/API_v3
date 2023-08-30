b7c7a23991f0ef244cc98270a5acb04515986a9b
ArnaudCHEVALME <arnaud.chevalme@proton.me>
2023-08-30 20:34:21 +0200

    fix cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 const corsOptions = {
   origin: "*",
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -22,13 +23,13 @@ const HOST = process.env._ABII_API_HOST;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "x-access-token, Origin, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // Use routes defined in backend/routers
 for (const createRouter of createRouterFunctions) {
